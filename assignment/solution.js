276fba0a18c097b11774fbcb62198ff88465f5e7
Zeyad Hesham <zeyad.tewila@student.giu-uni.de>
2026-09-23 17:13:33 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,9 @@
+const heroName = "Zeyad Hesham the Mage";
+const studentId = 16006822;
+const power = studentId * 7;
+const warcry = "By the Red Sea coast, the Basilisk crumbles!";
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log(warcry);
